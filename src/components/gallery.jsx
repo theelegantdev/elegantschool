f9a39d1fd34568295ed10ba31638a507062189f5
E-dev-ai <elegantschoolinfo@gamil.com>
2025-04-28 00:10:13 +0500

    added initial website

---
+++ src/components/gallery.jsx
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Gallery</h2>
+          <p>
+          Step into the world of The Elegant System of Education through our photo gallery. See how we celebrate achievement, creativity, and growth in every moment. Whether it’s academic milestones or school events, our gallery showcases the pride we take in our students' journeys
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
