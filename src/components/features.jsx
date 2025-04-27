152ec52966143656f6effeeced6c6238d2b82bf4
E-dev-ai <elegantschoolinfo@gamil.com>
2025-04-28 02:26:44 +0500

    fix features

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -10,8 +10,11 @@ export const Features = (props) => {
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
+                <div 
+                  key={`${d.title}-${i}`} 
+                  className="col-xs-12 col-sm-6 col-lg-3"
+                  style={{ marginBottom: "30px" }}
+                >
                   <i className={d.icon}></i>
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
